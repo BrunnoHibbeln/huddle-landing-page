0e9eb343b058f2d246abbe81603996d11d365584
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-08-17 23:46:51 +0000

    adding `google fonts` and `breakpoints` in `app/page.js`

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,20 @@
 import "./globals.css";
 
+import { Poppins } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "600"],
+});
+
+const opensans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-opensans",
+  weight: ["400"],
+});
+
 export const metadata = {
   title: "Huddle landing page",
 };
@@ -7,8 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head></head>
-      <body>{children}</body>
+      <body className={`${(poppins.variable, opensans.variable)}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["var(--font-poppins)"],
+        secondary: ["var(--font-opensans)"],
+      },
+      colors: {
+        violet: "hsl(257, 40%, 49%)",
+        magenta: "hsl(300, 69%, 71%)",
+      },
+      backgroundImage: {
+        desktop: "url('./images/bg-desktop.svg')",
+        mobile: "url('./images/bg-mobile.svg')",
+      },
+      screens: {
+        sm: "375px",
+        xl: "1440px",
+      },
+    },
   },
   plugins: [],
 };
