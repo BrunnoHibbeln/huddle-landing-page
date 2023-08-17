311b39182c5901162a8288787d2dd458d14c7db4
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-08-17 00:02:58 +0000

    adding `starter files` and `next app`

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Huddle landing page",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
